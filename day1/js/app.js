31730fa3496c0aadf3d6bec1ff089ea4e31491c2
Ian Keogh <iankeogh@Ians-MacBook-Pro.local>
2015-12-17 19:25:12 -0800

    first commit

---
+++ day1/js/app.js
@@ -0,0 +1,30 @@
+// // Ask for your first name
+// var fName = prompt('What is your first name?');
+
+// // Ask for your last name
+// var lName = prompt('What is your last name?');
+
+// // Log your first name to the console
+// console.log(fName); 
+
+// // Alert your last name
+// alert('Hello ' + lName);
+
+// // Confirm (anything) and alert the result of the confirm
+// confirm('I am afraid I cannot let you do that Dave');
+
+// // Include your app.js file in a blank empty html page
+// // Execute your script
+
+var a = 10,prompt('Type and integer.');
+
+var b = 10,prompt('Type another integer.');
+
+console.log(a,b);
+
+alert(a + b);
+
+confirm('Ta Dah!');
+
+
+
